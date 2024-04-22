51646295d2fa9819516d63b06a76b4c0413ace42
Paul Ilami <108613093+Paulilami@users.noreply.github.com>
2024-04-22 22:11:44 +0200

    Create index1.tsx

---
+++ index1.tsx
@@ -0,0 +1,6 @@
+export { default as ConnectWallet } from './ConnectWallet';
+export { default as TokenDropdown } from './TokenDropdown';
+export { default as TokenInput } from './TokenInput';
+export { default as RecipientInput } from './RecipientInput';
+export { default as TransferButton } from './TransferButton';
+export { default as Transfer } from './Transfer';
